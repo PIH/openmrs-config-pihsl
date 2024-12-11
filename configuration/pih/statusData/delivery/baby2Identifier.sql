ac0ceb9ad32500cb4b7fde024da0344ec03ea7c4
dmdesimone <ddesimone@pih.org>
2024-12-11 15:06:29 -0500

    SL-737-add-delivery-widget

--- configuration/pih/statusData/delivery/baby2Identifier.sql
+++ configuration/pih/statusData/delivery/baby2Identifier.sql
@@ -8,4 +8,14 @@ set @baby2UUID = obs_from_group_id_comment(@newborn2ObsGroupId, 'PIH','20150');
 set @baby2PatientId = (select person_id from person where UUID = @baby2UUID);
 set @baby2Identifier = patient_identifier(@baby2PatientId, 'c09a1d24-7162-11eb-8aa6-0242ac110002');
 
-select @baby2Identifier as baby2Identifier; 
+set @admissionEncounterType = encounterName('260566e1-c909-4d61-a96f-c1019291a09d');
+set @transferEncounterTyoe =  encounterName('436cfe33-6b81-40ef-a455-f134a9f7e580');
+set @dischargeEncounterType =  encounterName('b6631959-2105-49dd-b154-e1249e0fbcd7');
+set @latestDischargeEncounterId = latestEnc(@baby2PatientId, @dischargeEncounterType, null);
+set @latestDischargeDatetime = encounter_date(@latestDischargeEncounterId);
+set @latestAdmitEncounterId = latestEnc(@baby2PatientId, CONCAT(@admissionEncId,',',@transferEndId), @latestDischargeDatetime);
+set @admitLocation = encounter_location_name(@latestAdmitEncounterId);
+
+select @baby2Identifier as baby2Identifier,
+       if(@baby2Identifier is null,0,1) as baby2exists,  		
+       @admitLocation as admissionLocation; 
