4a4ec7d140856dd9f3f4a0b317af8fb26c9e96e3
dmdesimone <ddesimone@pih.org>
2024-12-12 10:43:11 -0500

    SL-737-add-delivery-widget

--- configuration/pih/statusData/delivery/baby3Identifier.sql
+++ configuration/pih/statusData/delivery/baby3Identifier.sql
@@ -3,8 +3,8 @@ set @pregnancyProgram = program('Pregnancy');
 set @pregnancyProgramId = mostRecentPatientProgramId(@patientId,@pregnancyProgram);
 set @pregnancyProgramStartDate = programStartDate(@pregnancyProgramId);
 set @latestDeliveryEncounterId = latestEnc(@patientId, @deliveryEncounterType, @pregnancyProgramStartDate);
-set @newborn3ObsGroupId = obs_id(@latestDeliveryEncounterId, 'PIH','13555',2);
-set @baby3UUID = obs_from_group_id_comment(@newborn3ObsGroupId, 'PIH','20150');
+set @baby3ObsGroupId = obs_id(@latestDeliveryEncounterId, 'PIH','13555',2);
+set @baby3UUID = obs_from_group_id_comment(@baby3ObsGroupId, 'PIH','20150');
 set @baby3PatientId = (select person_id from person where UUID = @baby3UUID);
 set @baby3Identifier = patient_identifier(@baby3PatientId, 'c09a1d24-7162-11eb-8aa6-0242ac110002');
 
