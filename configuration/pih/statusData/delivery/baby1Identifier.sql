cba27be1280e85e80c17993674ac4579c00cad52
dmdesimone <ddesimone@pih.org>
2024-12-17 20:23:49 -0500

    SL-737 Delivery Widget

--- configuration/pih/statusData/delivery/baby1Identifier.sql
+++ configuration/pih/statusData/delivery/baby1Identifier.sql
@@ -3,7 +3,7 @@ set @pregnancyProgram = program('Pregnancy');
 set @pregnancyProgramId = mostRecentPatientProgramId(@patientId,@pregnancyProgram);
 set @pregnancyProgramStartDate = programStartDate(@pregnancyProgramId);
 set @latestDeliveryEncounterId = latestEnc(@patientId, @deliveryEncounterType, @pregnancyProgramStartDate);
-set @baby1ObsGroupId = obs_id(@latestDeliveryEncounterId, 'PIH','13555',0);
+set @baby1ObsGroupId = obs_id_ordered_ascending(@latestDeliveryEncounterId, 'PIH','13555',0);
 set @baby1UUID = obs_from_group_id_comment(@baby1ObsGroupId, 'PIH','20150');
 set @baby1PatientId = (select person_id from person where UUID = @baby1UUID);
 set @baby1Identifier = patient_identifier(@baby1PatientId, 'c09a1d24-7162-11eb-8aa6-0242ac110002');
