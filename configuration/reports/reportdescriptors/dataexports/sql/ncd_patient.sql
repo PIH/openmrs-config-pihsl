91e29677a4c844cc50697cbfa44c4faa5f910bf1
dmdesimone <ddesimone@pih.org>
2025-04-24 15:23:13 -0400

    Sl 891 standardize column names (#222)
    * SL-891 standardize column names
    
    * SL-915 add new ncd_patient_fields

--- configuration/reports/reportdescriptors/dataexports/sql/ncd_patient.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/ncd_patient.sql
@@ -12,46 +12,50 @@ SET @partition = '${partitionNum}';
 
 DROP TEMPORARY TABLE IF EXISTS ncd_patient;
 CREATE TEMPORARY TABLE ncd_patient (
-patient_id int, 
-emr_id varchar(50),
-hiv varchar(255),
-comorbidities varchar(255),
-diabetes boolean,
-hypertension  boolean,
-heart_failure boolean,
-chronic_lung_disease boolean,
-chronic_kidney_disease boolean,
-liver_cirrhosis_hepb boolean,
-palliative_care boolean,
-sickle_cell boolean,
-other_ncd boolean,
-diabetes_type varchar(100),
-hypertension_type varchar(255), 
-rheumatic_heart_disease boolean, 
-congenital_heart_disease boolean,
-nyha_classification varchar(255),
-lung_disease_type varchar(255), 
-ckd_stage varchar(255),
-sickle_cell_type varchar(100),
-recent_program_id int,
-next_appointment_date date,
-dead boolean, 
-date_of_death date,
-name varchar(50), 
-family_name varchar(50),
-birthdate date,
-birthdate_estimated boolean,
-current_age int, 
-gender varchar(10),
-other_ncd_type text,
-most_recent_visit_date date,
-first_ncd_visit_date date,
-disposition varchar(255),
-ever_missed_school boolean,
-cardiomyopathy boolean,
-most_recent_hba1c_value int,
-most_recent_hba1c_date date,
-most_recent_echocardiogram_date date
+patient_id                      int,           
+emr_id                          varchar(50),  
+hiv                             varchar(255), 
+education_level                 varchar(255), 
+read_and_write                  bit,          
+referred_from                   text,         
+sickle_cell_confirmatory_test   varchar(255), 
+comorbidities                   varchar(255), 
+diabetes                        boolean,      
+hypertension                    boolean,      
+heart_failure                   boolean,      
+chronic_lung_disease            boolean,      
+chronic_kidney_disease          boolean,      
+liver_cirrhosis_hepb            boolean,      
+palliative_care                 boolean,      
+sickle_cell                     boolean,      
+other_ncd                       boolean,      
+diabetes_type                   varchar(100), 
+hypertension_type               varchar(255),  
+rheumatic_heart_disease         boolean,       
+congenital_heart_disease        boolean,      
+nyha_classification             varchar(255), 
+lung_disease_type               varchar(255),  
+ckd_stage                       varchar(255), 
+sickle_cell_type                varchar(100), 
+recent_program_id               int,          
+next_appointment_date           date,         
+dead                            boolean,       
+date_of_death                   date,         
+name                            varchar(50),   
+family_name                     varchar(50),  
+birthdate                       date,         
+birthdate_estimated             boolean,      
+current_age                     int,           
+gender                          varchar(10),  
+other_ncd_type                  text,         
+most_recent_visit_date          date,         
+first_ncd_visit_date            date,         
+disposition                     varchar(255), 
+ever_missed_school              boolean,      
+cardiomyopathy                  boolean,      
+most_recent_hba1c_value         int,          
+most_recent_hba1c_date          date,         
+most_recent_echocardiogram_date date          
 );
 
 
@@ -136,6 +140,19 @@ birthdate(patient_id)
 FROM temp_encounter;
 
 create index ncd_patient_i1 on ncd_patient(patient_id); 
+
+-- Patient details columns
+UPDATE ncd_patient
+SET education_level = last_value_coded_list_from_temp(patient_id, 'PIH', '1688','en');
+
+UPDATE ncd_patient
+SET read_and_write = value_coded_as_boolean(latest_obs_from_temp(patient_id, 'PIH','13736'));
+
+UPDATE ncd_patient
+SET referred_from = last_value_coded_list_from_temp(patient_id, 'PIH', '7454','en');
+
+UPDATE ncd_patient
+SET sickle_cell_confirmatory_test = last_value_coded_list_from_temp(patient_id, 'PIH', '15143','en');
 
 UPDATE ncd_patient tgt 
 INNER JOIN last_echocardiogram lc ON tgt.patient_id = lc.patient_id
@@ -266,6 +283,9 @@ SELECT
 if(@partition REGEXP '^[0-9]+$' = 1,concat(@partition,'-',patient_id),patient_id) "patient_id",
 emr_id,
 hiv,
+education_level,
+read_and_write,
+referred_from,
 comorbidities,
 diabetes,
 hypertension,
@@ -284,6 +304,7 @@ nyha_classification,
 lung_disease_type,
 ckd_stage,
 sickle_cell_type,
+sickle_cell_confirmatory_test,
 cast(next_appointment_date as date) as next_appointment_date,
 dead,
 cast(date_of_death as date) as date_of_death,
