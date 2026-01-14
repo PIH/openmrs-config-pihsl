de925df6c0bff3b8038b4b79d038dc43308328e5
dmdesimone <ddesimone@pih.org>
2026-01-14 09:36:51 -0500

    UHM-9025-Update-maternalHandoff-report-with-new-parameter

--- configuration/reports/reportdescriptors/dataexports/sql/maternal_handoff_death_list.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/maternal_handoff_death_list.sql
@@ -1,4 +1,19 @@
--- set @handoverDate = '2026-01-13';
+-- set @handoverDate = '2026-01-14';
+-- set @shift = 'morning'; -- 'evening' ;
+
+set @startTime = 
+case
+	when @shift = 'morning' then DATE_SUB(@handoverDate, INTERVAL 1 DAY) + INTERVAL 8 HOUR
+	when @shift = 'evening' then @handoverDate + INTERVAL 8 HOUR
+end;
+
+set @endTime = 
+case
+	when @shift = 'morning' then @handoverDate + INTERVAL 8 HOUR
+	when @shift = 'evening' then @handoverDate + INTERVAL 20 HOUR	
+end;
+
+
 set @identifier_uuid = metadata_uuid('org.openmrs.module.emrapi', 'emr.primaryIdentifierType');
 select program_id into @pregnancy_program_id from program p where uuid = '6a5713c2-3fd5-46e7-8f25-36a0f7871e12';
 select location_id into @anc from location where uuid = '11f5c9f9-40b8-46ad-9e7e-59473ce43246';
@@ -36,10 +51,13 @@ insert into temp_deaths(patient_id, age_of_death, death_datetime, cause)
 select p.person_id, TIMESTAMPDIFF(YEAR, p.birthdate, death_date),
 p.death_date, concept_name(p.cause_of_death, @locale)  
 from person p 
- inner join visit v on v.patient_id = p.person_id and v.date_started <= p.death_date and date(v.date_stopped) >= date(p.death_date) 
-where p.death_date >= DATE_SUB(@handoverDate, INTERVAL 1 YEAR) + INTERVAL 8 HOUR
-and p.death_date <  @handoverDate + INTERVAL 8 HOUR
- and exists 
+inner join visit v on v.visit_id = 
+	(select v2.visit_id from visit v2
+	where v2.patient_id = p.person_id
+	order by v2.date_started desc limit 1)
+where p.death_date >= @startTime
+and p.death_date <  @endTime
+and exists 
 	(select 1 from encounter e 
 	where e.visit_id = v.visit_id
 	and e.location_id in (@anc, @labour, @nicu, @pacu, @pnc, @quiet, @mccu, @postop, @preop, @mcoe_triage));
@@ -89,9 +107,9 @@ select
 emr_id "EMR_Id",
 patient_name "Patient_name",
 age_of_death "Age_of_Death",
-death_datetime "Time_of_Death",
+  CONCAT(DATE_FORMAT(death_datetime, '%b %e, %Y '), LTRIM(LOWER(DATE_FORMAT(death_datetime, '%l:%i%p')))) "Time_of_Death",
 cause "Cause_of_Death",
 pregnancy_status "Pregnancy_Status",
 gestational_age "Gestational_Age"
 from temp_deaths
-order by death_datetime ;
+order by death_datetime;
