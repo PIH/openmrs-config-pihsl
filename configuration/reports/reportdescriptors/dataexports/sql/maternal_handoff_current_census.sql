730291267b0524d48e650ed24964c43ade9248b8
dmdesimone <ddesimone@pih.org>
2026-02-18 22:06:57 -0500

    SL-1205 combine/split locations on census handoff (#290)

--- configuration/reports/reportdescriptors/dataexports/sql/maternal_handoff_current_census.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/maternal_handoff_current_census.sql
@@ -13,19 +13,25 @@ select location_id into @postop from location where uuid = 'a39ec469-d1f9-11f0-9
 select location_id into @preop from location where uuid = '142de844-6850-11ee-ab8d-0242ac120002';
 select location_id into @kmc from location where uuid = '81080213-d1f9-11f0-9d46-169316be6a48';
 select location_id into @mothers from location where uuid = '989a9b23-d1f9-11f0-9d46-169316be6a48';
+select location_id into @mcoe_triage from location where uuid = 'f85feffc-fe54-4648-aa14-01ed6d30b943';
 
 drop temporary table if exists temp_census;
 create temporary table temp_census
+(location_id int(11),
+patient_id int(11),
+bed_number varchar(50));
+
+insert into temp_census (location_id, patient_id)
 (select distinct e.location_id, e.patient_id from encounter e 
 inner join visit v on e.visit_id = v.visit_id AND v.date_stopped is null 
 where e.voided = 0
 and e.encounter_type in (@admission, @transfer)
-and e.location_id in (@anc, @labour, @nicu, @pacu, @pnc, @quiet, @mccu, @postop, @preop, @kmc, @mothers)
+and e.location_id in (@anc, @labour, @nicu, @pacu, @pnc, @quiet, @mccu, @postop, @kmc, @mothers, @mcoe_triage)
 and not exists 
 	(select 1 from encounter e2
 	where e2.voided = 0
 	and e2.patient_id = e.patient_id 
-	and (e2.encounter_type in (@exit_from_care) or (e2.encounter_type = @transfer and e2.location_id <> e.location_id))
+	and (e2.encounter_type in (@exit_from_care) or (e2.encounter_type in (@transfer, @admission) and e2.location_id <> e.location_id))
 	and e2.encounter_datetime > e.encounter_datetime 
 	));
 
@@ -34,6 +40,11 @@ inner join person p on p.person_id = t.patient_id
 where (p.birthdate is null or (TIMESTAMPDIFF(YEAR, p.birthdate, CURDATE()) < 10 and t.location_id <> @nicu))
    or (p.birthdate is null or (TIMESTAMPDIFF(YEAR, p.birthdate, CURDATE()) > 0 and t.location_id = @nicu)) ;
 
+update temp_census t 
+inner join bed_patient_assignment_map m on m.patient_id = t.patient_id
+inner join bed b on b.bed_id = m.bed_id 
+set t.bed_number = b.bed_number;
+
 drop temporary table if exists temp_final;
 create temporary table temp_final 
 (ward varchar(255),
@@ -41,19 +52,44 @@ current_census int,
 free_beds int);
 
 INSERT INTO temp_final (ward)
-(select name from location where location_id in(@anc, @labour, @nicu, @pacu, @pnc, @quiet, @mccu, @postop, @preop, @kmc, @mothers));
+(select name from location where location_id in(@anc, @labour, @nicu, @pnc, @quiet, @mccu, @postop, @kmc, @mothers, @mcoe_triage));
+INSERT INTO temp_final (ward)
+VALUES
+('PACU/Pre-Op'),
+('NICU Inborn'),
+('NICU Outborn'),
+('NICU Other'),
+('NICU Unassigned');
 
 update temp_final t 
 inner join  
-	(select name "ward", count(*) "census"
+	(select 
+	case
+		when l.location_id in (@preop, @pacu) then 'PACU/Pre-Op'
+		when l.location_id = @nicu and bed_number like '%inborn%' then 'NICU Inborn'
+		when l.location_id = @nicu and bed_number like '%outborn%' then 'NICU Outborn'
+		when l.location_id = @nicu and bed_number is not null then 'NICU Other'		
+		when l.location_id = @nicu and bed_number is null then 'NICU Unassigned'
+		else name
+	end "ward",
+	count(*) "census"
 	from temp_census t  
 	inner join location l on l.location_id = t.location_id
-	group by name) i on i.ward = t.ward 
+	group by ward) i on i.ward = t.ward 
 set t.current_census = i.census;
 
 update temp_final t 
 inner join  
-	(select l.name "ward", count(*) "free_beds" 
+	(select 
+	case
+		when l.location_id in (@preop, @pacu) then 'PACU/Pre-Op'
+		when l.location_id = @nicu and bed_number like '%inborn%' then 'NICU Inborn'
+		when l.location_id = @nicu and bed_number like '%outborn%' then 'NICU Outborn'
+		when l.location_id = @nicu and bed_number is not null then 'NICU Other'		
+		when l.location_id = @nicu and bed_number is null then 'NICU Unassigned'
+		else name
+	end "ward",
+	count(*) "free_beds" 
 	from bed_location_map blm
 	inner join bed b on b.bed_id = blm.bed_id and b.voided = 0
 	inner join location l on l.location_id = blm.location_id
@@ -62,11 +98,16 @@ inner join
 		where bpam.bed_id = blm.bed_id  
 		and bpam.voided = 0
 		and bpam.date_stopped is null)
-	group by l.name) i on i.ward = t.ward
+	group by ward) i on i.ward = t.ward
 set t.free_beds = i.free_beds;
 
 update temp_final t set t.current_census = 0 where t.current_census is null;
 update temp_final t set t.free_beds = 0 where t.free_beds is null;
+
+delete from temp_final
+where current_census = 0 
+and free_beds = 0 
+and ward in ('NICU Other','NICU Unassigned');
 
 select 
 ward "Ward",
