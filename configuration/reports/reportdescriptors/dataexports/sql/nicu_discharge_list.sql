b65d477e56ba4710f439c058daad2481806dfb5a
mogoodrich <mgoodrich@pih.org>
2026-05-15 16:28:08 -0400

    SL-1337:Change NICU Discharge list to use visit attribute

--- configuration/reports/reportdescriptors/dataexports/sql/nicu_discharge_list.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/nicu_discharge_list.sql
@@ -3,6 +3,7 @@ SET @locale = GLOBAL_PROPERTY_VALUE('default_locale', 'en');
 
 select encounter_type_id into @newborn_discharge from encounter_type where uuid = '153d3182-c76f-4047-b7f2-d83cf967b206';
 select location_id into @nicu_location from location where uuid = '0ce2f6fb-6850-11ee-ab8d-0242ac120002';
+select visit_attribute_type_id into @bornDuringVisitAttribute from visit_attribute_type where uuid = '86f716fc-5e26-4eb1-9484-46370cff28f0';
 
 DROP TABLE IF EXISTS temp_discharge_list;
 CREATE TABLE temp_discharge_list
@@ -35,26 +36,14 @@ update temp_discharge_list
 set name = person_name(patient_id);
 
 update temp_discharge_list
-set discharge_provider = provider(encounter_id); 
+set discharge_provider = provider(encounter_id);
 
-set @patient_exists = concept_from_mapping('PIH','20150');
-set @yes = concept_from_mapping('PIH','1065');
-set @delivery_summary = (select encounter_type_id from encounter_type where uuid = 'fec2cc56-e35f-42e1-8ae3-017142c1ca59');
-
-DROP TABLE IF EXISTS temp_inborn_baby_birthdates;
-CREATE TABLE temp_inborn_baby_birthdates 
-(select t.patient_id, o.obs_datetime "birthdatetime"  from temp_discharge_list t
-inner join person p on p.person_id = t.patient_id
-inner join obs o on o.comments = p.uuid 
-	and o.concept_id = @patient_exists
-	and o.value_coded = @yes);
-
-update temp_discharge_list t  
-inner join visit v on v.visit_id = t.visit_id
-inner join temp_inborn_baby_birthdates bb on bb.patient_id = t.patient_id 
-	and bb.birthdatetime >= v.date_started 
-	and (bb.birthdatetime <= v.date_stopped or v.date_stopped is null)
-set t.inborn_outborn = 'inborn';	
+update temp_discharge_list t
+inner join visit_attribute va on va.visit_id = t.visit_id
+    and va.attribute_type_id = @bornDuringVisitAttribute
+    and va.value_reference = 'true'
+    and va.voided = 0
+set t.inborn_outborn = 'inborn';
 update temp_discharge_list t set inborn_outborn = 'outborn' where t.inborn_outborn is null;
 
 
